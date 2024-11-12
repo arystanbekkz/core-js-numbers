ec58f8578c9cf4c7dfce00612c977379fcf30441
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2024-11-12 17:04:36 +0500

    feat: add getSumOfDigits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -288,8 +288,10 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return String(num)
+    .split('')
+    .reduce((prev, curr) => prev + +curr, 0);
 }
 
 /**
